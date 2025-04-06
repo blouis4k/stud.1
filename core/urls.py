d0d16ffd45e95eeeed521e3f22f79cc5cded8779
blouis4k <a.louistucker@gmail.com>
2025-04-06 15:43:09 +0100

    Update urls.py V7

--- core/urls.py
+++ core/urls.py
@@ -1,4 +1,11 @@
+from django.contrib import admin
+from django.urls import path
 from django.http import HttpResponse
 
 def setup_admin(request):
     return HttpResponse("Hello! This route is working.")
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('setup/', setup_admin),
+]
