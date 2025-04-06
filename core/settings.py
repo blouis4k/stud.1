06b9e1f12f9029baa2d834d7ce425df6217f8fc9
blouis4k <a.louistucker@gmail.com>
2025-04-06 16:33:50 +0100

    Fix admin config and enable middleware

--- core/settings.py
+++ core/settings.py
@@ -1,4 +1,4 @@
-# Basic working Django settings
+# Django settings for Create Studios backend
 
 ROOT_URLCONF = 'core.urls'
 
@@ -17,3 +17,29 @@ DATABASES = {
         'NAME': 'mydatabase',
     }
 }
+
+MIDDLEWARE = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
+
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
