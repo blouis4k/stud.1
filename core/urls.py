ae81637738008b33c992a04f82ca7aa44e0e18e1
blouis4k <a.louistucker@gmail.com>
2025-04-06 14:52:31 +0100

    Update urls.py V5

--- core/urls.py
+++ core/urls.py
@@ -1,3 +1,5 @@
+from django.contrib import admin
+from django.urls import path
 from django.contrib.auth import get_user_model
 from django.http import HttpResponse
 from django.core.management import call_command
@@ -7,8 +9,6 @@ def setup_admin(request):
     try:
         return HttpResponse("Setup started. Running migrate...")
         call_command('migrate')
-        return HttpResponse("Migrations successful. Creating superuser...")
-
         User = get_user_model()
         if not User.objects.filter(username='admin').exists():
             User.objects.create_superuser(
@@ -20,3 +20,8 @@ def setup_admin(request):
         return HttpResponse("Admin already exists.")
     except Exception:
         return HttpResponse('<pre>' + traceback.format_exc() + '</pre>', status=500)
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('setup/', setup_admin),
+]
