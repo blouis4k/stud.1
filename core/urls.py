8e65775853ab5c5ce1bb4649d4993ff0eb5caeef
blouis4k <a.louistucker@gmail.com>
2025-04-06 15:55:23 +0100

    Update urls.py V9

--- core/urls.py
+++ core/urls.py
@@ -1,11 +1,20 @@
-from django.contrib import admin
-from django.urls import path
+from django.contrib.auth import get_user_model
+from django.core.management import call_command
 from django.http import HttpResponse
+import traceback
 
 def setup_admin(request):
-    return HttpResponse("Hello! This route is working.")
+    try:
+        call_command('migrate')
 
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('setup/', setup_admin),
-]
+        User = get_user_model()
+        if not User.objects.filter(username='admin').exists():
+            User.objects.create_superuser(
+                username='admin',
+                email='info@createstudiosleicester.co.uk',
+                password='CreateSecureAdmin2025!'
+            )
+            return HttpResponse("Superuser created! You can now log in at /admin/")
+        return HttpResponse("Admin already exists. You can log in at /admin/")
+    except Exception:
+        return HttpResponse('<pre>' + traceback.format_exc() + '</pre>', status=500)
