addb0abc7494b8d33b8037b4daf221f22e8c73f8
blouis4k <a.louistucker@gmail.com>
2025-04-10 19:21:38 +0100

    Update admin.py

--- booking/admin.py
+++ booking/admin.py
@@ -1,1 +1,6 @@
-admin
+from django.contrib import admin
+from .models import Booking
+
+@admin.register(Booking)
+class BookingAdmin(admin.ModelAdmin):
+    list_display = ('studio_name', 'date', 'start_time', 'end_time', 'is_block_booking')
