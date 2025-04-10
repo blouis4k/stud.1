aa2c18ddf8110718137768069ca4a38c6b3954e7
blouis4k <a.louistucker@gmail.com>
2025-04-10 19:19:09 +0100

    Update models.py

--- booking/models.py
+++ booking/models.py
@@ -1,1 +1,11 @@
-models
+from django.db import models
+
+class Booking(models.Model):
+    studio_name = models.CharField(max_length=100)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    is_block_booking = models.BooleanField(default=False)
+
+    def __str__(self):
+        return f"{self.studio_name} booking on {self.date} from {self.start_time} to {self.end_time}"
