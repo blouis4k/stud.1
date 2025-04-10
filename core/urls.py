2e976eae8f9d0dc675921661aa21cfccf3963302
blouis4k <a.louistucker@gmail.com>
2025-04-10 19:58:04 +0100

    Update urls.py

--- core/urls.py
+++ core/urls.py
@@ -1,6 +1,11 @@
 from django.contrib import admin
 from django.urls import path
+from django.http import HttpResponse
+
+def home(request):
+    return HttpResponse("🎉 Create Studios backend is live and working!")
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', home),
 ]
